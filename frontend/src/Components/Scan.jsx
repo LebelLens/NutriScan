ba6f6b72c19d2e954fcfe658e2acaa0b181c9f08
Abhik-Mudi <abhikmudi6@gmail.com>
2025-12-10 16:28:00 +0530

    name changed

--- frontend/src/Components/Scan.jsx
+++ frontend/src/Components/Scan.jsx
@@ -6,7 +6,7 @@ const Scan = () => {
     return (
         <div className='md:w-[50vw] p-4 rounded-2xl m-auto my-24 border border-(--border) bg-(--background) shadow-[0_10px_30px_rgba(5, 150, 105, 0.3)]'>
             {/* QR section */}
-            <h1 className='text-2xl text-(--text) font-medium'>Scan the QR</h1>
+            <h1 className='text-2xl text-(--text) font-medium'>Scan the Barcode</h1>
             <p className='text-(--textLight)'>Scan food ingredients and make healthier choices</p>
             <div className='card mt-10 p-5 rounded-2xl flex flex-col items-center gap-5'>
                 <div className='rounded-full bg-white/40 p-5'><Camera className='text-lg text-white' /></div>
