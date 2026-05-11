97cec8aa4ae52a20e8ceecd65b501f3bcf20e1a4
Abhik-Mudi <abhikmudi6@gmail.com>
2026-05-11 13:23:23 +0530

    production changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -38,5 +38,11 @@ export default defineConfig({
 
   server: {
     port: 3000,
+    proxy: {
+      "/api":{
+        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
   },
 })
